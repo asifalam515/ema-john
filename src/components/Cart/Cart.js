26d532a41ca487e89d7a39ed0a8f793bb07ba613
asifalam515 <asibulalam515@gmail.com>
2022-11-13 11:03:31 +0600

    cart section created

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Cart = (props) => {
+    return (
+        <div>
+              <h3>Order Summary in Cart</h3>
+        <p>Selected Items:{props.cart.length}</p>
+            
+        </div>
+    );
+};
+
+export default Cart;
+ 
