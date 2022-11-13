4c00468d9adb7d99c583e262cac1163399e61d88
asifalam515 <asibulalam515@gmail.com>
2022-11-13 22:08:51 +0600

    relaod fixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
+    const {cart}=props
+    let total=0;
+    let shipping=0;
+
+    for(const product of cart){
+        total=total+product.price
+        shipping=shipping+product.shipping
+    }
+    const tax=(total*(10/100)).toFixed(2)
+    const grandTotal=total+shipping+parseFloat(tax)
+    
     return (
-        <div>
+        <div className='cart'>
               <h3>Order Summary in Cart</h3>
-        <p>Selected Items:{props.cart.length}</p>
+        <p>Selected Items:{cart.length}</p>
+        <p>Total Price:${total}</p>
+        <p>Total Shipping:${shipping}</p>
+        <p>Tax:${tax}</p>
+        <h3>Grand Total:${grandTotal}</h3>
             
         </div>
     );
